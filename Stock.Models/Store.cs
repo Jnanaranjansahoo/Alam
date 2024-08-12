5c5953ada6fb01b3e29f9dc6183520a60dcb2fe4
jnana <jnanaranjansahoo016@gmail.com>
2024-08-12 18:02:25 +0530

    f

---
+++ Stock.Models/Store.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc.ModelBinding.Validation;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace Stock.Models
+{
+    public class Store
+    {
+        public int Id { get; set; }
+        public int CategoryId { get; set; }
+        [ForeignKey("CategoryId")]
+        [ValidateNever]
+        public Category Category { get; set; }
+        
+        [Required]
+        public string Company { get; set; }
+        [Required]
+        public int Quantity { get; set; }
+        public int Defect { get; set; }
+        public int Total { get; set; }
+        public string Transport { get; set; }
+    }
+}
---
+++ Stock/Areas/Admin/Controllers/StoreController.cs
@@ -0,0 +1,144 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Stock.DataAcess.Data;
+using Stock.DataAcess.Repository.IRepository;
+using Stock.Models;
+using Stock.Models.ViewModels;
+
+namespace StockWeb.Areas.Admin.Controllers
+{
+    [Area("Admin")]
+    public class StoreController : Controller
+    {
+        private readonly IUnitOfWork _unitOfWork;
+        private readonly IWebHostEnvironment _webHostEnvironment;
+        public StoreController(IUnitOfWork unitOfWork, IWebHostEnvironment webHostEnvironment)
+        {
+            _unitOfWork = unitOfWork;
+            _webHostEnvironment = webHostEnvironment;
+        }
+        public IActionResult Index()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties:"Category").ToList();
+
+            //ViewBag.ProductCount = objProductList.Count;
+            //ViewBag.Total = objClientList.Count * 120;
+            return View(objProductList);
+        }
+
+        public IActionResult Upsert(int? id)
+        {
+            
+            ProductVM productVM = new()
+            {
+                CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                }),
+                Product = new Product()
+            };
+            if(id == null || id == 0)
+            {
+                // Create
+
+                return View(productVM);
+            }
+            else
+            {
+                // Uplode
+
+                productVM.Product = _unitOfWork.Product.Get(u=>u.Id == id);
+                return View(productVM);
+            }
+        }
+        [HttpPost]
+        public IActionResult Upsert(ProductVM productVM, IFormFile? file)
+        {
+            if (ModelState.IsValid)
+            {
+                string wwwRootPath = _webHostEnvironment.WebRootPath;
+                if (file != null)
+                {
+                    string fileName = Guid.NewGuid().ToString() + Path.GetExtension(file.FileName);
+                    string clientPath = Path.Combine(wwwRootPath, @"images\product");
+                    if (!string.IsNullOrEmpty(productVM.Product.ImageUrl))
+                    {
+                        // Delete the old images
+
+                        var oldImagePath =
+                            Path.Combine(wwwRootPath, productVM.Product.ImageUrl.TrimStart('\\'));
+
+
+                        if (System.IO.File.Exists(oldImagePath))
+                        {
+                            System.IO.File.Delete(oldImagePath);
+                        }
+                    }
+
+                    using (var fileStream = new FileStream(Path.Combine(clientPath, fileName), FileMode.Create))
+                    {
+                        file.CopyTo(fileStream);
+                    }
+
+                    productVM.Product.ImageUrl = @"\images\product\" + fileName;
+                }
+            
+                if (productVM.Product.Id == 0)
+                {
+                    _unitOfWork.Product.Add(productVM.Product);
+                }
+                else
+                {
+                    _unitOfWork.Product.Update(productVM.Product);
+                }
+                    
+                _unitOfWork.Save();
+                TempData["success"] = "Product Created Successfully";
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                productVM.CategoryList = _unitOfWork.Category.GetAll().Select(u => new SelectListItem
+                {
+                    Text = u.Name,
+                    Value = u.Id.ToString()
+                });
+                return View(productVM);
+            }
+        }
+      
+        #region API CALLS
+        [HttpGet]
+        public IActionResult GetAll()
+        {
+            List<Product> objProductList = _unitOfWork.Product.GetAll(includeProperties: "Category").ToList();
+            return Json(new { data = objProductList });
+        }
+        [HttpDelete]
+        public IActionResult Delete(int id)
+        {
+            var productToBeDeletede = _unitOfWork.Product.Get(u => u.Id == id);
+            if (productToBeDeletede == null)
+            {
+                return Json(new {success = false, message = "Error while deleting "});
+            }
+
+            var oldImagePath =
+                            Path.Combine(_webHostEnvironment.WebRootPath,
+                            productToBeDeletede.ImageUrl.TrimStart('\\'));
+
+
+            if (System.IO.File.Exists(oldImagePath))
+            {
+                System.IO.File.Delete(oldImagePath);
+            }
+
+            _unitOfWork.Product.Remove(productToBeDeletede);
+            _unitOfWork.Save();
+
+           return Json(new { success = true, message = "Delete Successful" });
+        }
+        #endregion
+    }
+}
